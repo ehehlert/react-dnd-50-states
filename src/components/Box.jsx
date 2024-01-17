3ff27902fd00c23c0c4bb8244e25db920b8228e0
eric <ehehlert1994@gmail.com>
2024-01-17 07:58:03 -0600

    Changing the layout of the app to use tailwindcss

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -12,20 +12,9 @@ export default function Box() {
   return (
     <div
       ref={drag}
-      style={{
-        border: isDragging ? "1px dashed gray" : "1px solid gray",
-        backgroundColor: "lightblue",
-        padding: "1rem",
-        marginRight: "0.5rem",
-        marginBottom: "0.5rem",
-        width: "100px",
-        height: "100px",
-        textAlign: "center",
-        alignItems: "center",
-        display: "flex",
-        justifyContent: "center",
-        color: isDragging ? "red" : "blue",
-      }}
+      className={`border ${
+        isDragging ? "border-dashed text-red-500" : "border-solid text-blue-500"
+      } bg-lightblue-200 p-4 mr-2 mb-2 w-24 h-24 flex items-center justify-center text-center`}
     >
       {isDragging ? "IM BEING DRAGGED" : "DRAG ME"}
     </div>
